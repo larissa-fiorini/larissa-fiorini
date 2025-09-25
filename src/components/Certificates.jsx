317e426154631833d9c7f74e3714cde9d6ed94c2
larissa-fiorini <larissafiorini00@gmail.com>
2025-09-25 15:35:00 -0300

    Improve mobile style

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -7,14 +7,14 @@ export function Certificates() {
           Certificates
         </h1>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:w-full lg:flex lg:flex-row lg:items-center justify-center">
-          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 w-full lg:flex lg:flex-row lg:items-center justify-center">
+          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg w-full flex items-center justify-center">
             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="0d3f0dcc-e250-431f-8a6b-5bc112a91465" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
           </div>
-          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg">
+          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg w-full flex items-center justify-center">
             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="1ac72032-1dfb-440a-acd7-fb17408ff147" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
           </div>
-          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg">
+          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg w-full flex items-center justify-center">
             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="cfdd6efc-5f3d-454e-9cbe-1917c3c629e2" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
           </div>
         </div>
