047ad296d8bbb906b4b4a3bf736fdfbd40ad6298
larissa-fiorini <larissafiorini00@gmail.com>
2026-01-07 10:34:33 -0300

    Adding certificate

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -17,6 +17,9 @@ export function Certificates() {
           <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg w-full flex items-center justify-center">
             <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="cfdd6efc-5f3d-454e-9cbe-1917c3c629e2" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
           </div>
+          <div className="bg-slate-400 m-4 p-2 rounded-lg shadow-lg w-full flex items-center justify-center">
+            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="0ebeeb5d-8dbf-43c4-90cf-aa6260c8b874" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+          </div>
         </div>
       </div>
     </div>
